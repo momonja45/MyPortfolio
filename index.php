28ec5de050af6d0d593e11ada80f6b9306f3588b
momonja45 <vmonvincent@gmail.com>
2023-02-04 21:51:53 +0800

    Fonts and colors

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+$wew = "wew";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@500;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+    <title>Momonja</title>
+</head>
+<body>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro omnis hic corrupti delectus ab dicta minus sint animi exercitationem dignissimos mollitia distinctio earum repellendus, soluta amet fugiat odio. Tempora deserunt voluptas vitae, iste ex necessitatibus. Quasi et veniam consequuntur ut ipsum repellendus autem quidem odit dolorem dignissimos vero, natus, alias cumque culpa pariatur. Aut magnam et, velit, expedita ab est impedit nulla eveniet iste exercitationem, accusantium cum eligendi eius odit commodi necessitatibus sequi quam blanditiis. Praesentium facilis minima impedit magni quam temporibus excepturi. Animi, tenetur. Aliquid amet incidunt placeat voluptatibus possimus exercitationem, blanditiis, aliquam atque saepe reiciendis nostrum optio natus?
+</body>
+</html>
